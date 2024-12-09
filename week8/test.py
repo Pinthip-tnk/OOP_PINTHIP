7feb68cb39d377ba8482299f6b493dd9e6cc62b7
Pinthip-tnk <67319010061@tnk.ac.th>
2024-12-09 14:29:11 +0700

    pinthip

---
+++ week8/test.py
@@ -0,0 +1,9 @@
+class Cat:
+    def __inti__(self,cat_name,cat_color):
+        self.name = cat_name
+        self.color = cat_color
+    def Cry(self):
+        print("เมี้ยวๆ")
+mycat1 = Cat("ศรีทอง","สีส้ม")     
+mycat2 = Cat("ศรีเงิน","ศรีขาว")   
+mycat1.cry()
